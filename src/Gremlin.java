bda2f86ac6f393da446a31a9a21b3b7be39b8f37
Kevin Underwood <kunderwood1@eagles.ewu.edu>
2020-02-27 21:22:56 -0800

    Add files via upload
    removed outdated Keyboard class 
    removed the static method playAgain - lazy class. It didn't pull its own weight.

---
+++ src/Gremlin.java
@@ -0,0 +1,30 @@
+
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author
+ * @version 1.0
+ */
+
+public class Gremlin extends Monster
+{
+
+    public Gremlin()
+	{
+		super("Gnarltooth the Gremlin", 70, 5, .8, .4, 15, 30, 20, 40);
+
+    }//end constructor
+
+	public void attack(DungeonCharacter opponent)
+	{
+		System.out.println(name + " jabs his kris at " +
+							opponent.getName() + ":");
+		super.attack(opponent);
+
+	}//end override of attack
+
+
+}//end class Gremlin
---
+++ src/Monster.java
@@ -0,0 +1,60 @@
+
+
+/**
+ * Title:
+ * Description:
+ * Copyright:    Copyright (c) 2001
+ * Company:
+ * @author
+ * @version 1.0
+ */
+
+
+public abstract class Monster extends DungeonCharacter
+{
+	protected double chanceToHeal;
+	protected int minHeal, maxHeal;
+
+//-----------------------------------------------------------------
+  public Monster(String name, int hitPoints, int attackSpeed,
+				     double chanceToHit, double chanceToHeal,
+					 int damageMin, int damageMax,
+					 int minHeal, int maxHeal)
+  {
+	super(name, hitPoints, attackSpeed, chanceToHit, damageMin, damageMax);
+	this.chanceToHeal = chanceToHeal;
+	this.maxHeal = maxHeal;
+	this.minHeal = minHeal;
+
+  }//end monster construcotr
+
+//-----------------------------------------------------------------
+  public void heal()
+  {
+	boolean canHeal;
+	int healPoints;
+
+	canHeal = (Math.random() <= chanceToHeal) && (hitPoints > 0);
+
+	if (canHeal)
+	{
+		healPoints = (int)(Math.random() * (maxHeal - minHeal + 1)) + minHeal;
+		addHitPoints(healPoints);
+		System.out.println(name + " healed itself for " + healPoints + " points.\n"
+							+ "Total hit points remaining are: " + hitPoints);
+		System.out.println();
+	}//end can heal
+
+
+  }//end heal method
+
+//-----------------------------------------------------------------
+ public void subtractHitPoints(int hitPoints)
+ {
+		super.subtractHitPoints(hitPoints);
+		heal();
+
+ }//end method
+
+
+}//end Monster class
