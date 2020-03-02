feab19b04ad840824784b476ab36a29e90312ea5
Kevin Underwood <kunderwood1@eagles.ewu.edu>
2020-03-01 17:34:39 -0800

    Add files via upload

--- Ogre.java
+++ Ogre.java
@@ -1,31 +1,25 @@
-
-
 /**
  * Title:
  * Description:
  * Copyright:    Copyright (c) 2001
  * Company:
+ *
  * @author
  * @version 1.0
  */
 
-public class Ogre extends Monster
-{
+public class Ogre extends Monster {
 
-    public Ogre()
-	{
-		super("Oscar the Ogre", 200, 2, .6, .1, 30, 50, 30, 50);
+    public Ogre() {
+        super("Oscar the Ogre", 200, 2, .6, .1, 30, 50, 30, 50);
+    }
 
+    public void attack(DungeonCharacter opponent) {
+        System.out.println(name + " slowly swings a club toward's " +
+                opponent.getName() + ":");
+        super.attack(opponent);
 
-    }//end constructor
-
-	public void attack(DungeonCharacter opponent)
-	{
-		System.out.println(name + " slowly swings a club toward's " +
-							opponent.getName() + ":");
-		super.attack(opponent);
-
-	}//end override of attack
+    }//end override of attack
 
 
 }//end Monster class
--- Warrior.java
+++ Warrior.java
@@ -5,82 +5,65 @@ import java.util.Scanner;
  * Description:
  * Copyright:    Copyright (c) 2001
  * Company:
+ *
  * @author
  * @version 1.0
  */
 
 
+public class Warrior extends Hero {
+
+    public Warrior() {
+        super("Warrior", 125, 4, .8, 35, 60, .2);
+    }
 
 
-public class Warrior extends Hero
-{
+    public void crushingBlow(DungeonCharacter opponent) {
+        if (Math.random() <= .4) {
+            int blowPoints = (int) (Math.random() * 76) + 100;
+            System.out.println(name + " lands a CRUSHING BLOW for " + blowPoints
+                    + " damage!");
+            opponent.subtractHitPoints(blowPoints);
+        }
+        else {
+            System.out.println(name + " failed to land a crushing blow");
+            System.out.println();
+        }
 
-    public Warrior()
-	{
+    }
 
-		super("Warrior", 125, 4, .8, 35, 60, .2);
-
-
-    }//end constructor
-
-
-	public void crushingBlow(DungeonCharacter opponent)
-	{
-		if (Math.random() <= .4)
-		{
-			int blowPoints = (int)(Math.random() * 76) + 100;
-			System.out.println(name + " lands a CRUSHING BLOW for " + blowPoints
-								+ " damage!");
-			opponent.subtractHitPoints(blowPoints);
-		}//end blow succeeded
-		else
-		{
-			System.out.println(name + " failed to land a crushing blow");
-			System.out.println();
-		}//blow failed
-
-	}//end crushingBlow method
-
-	public void attack(DungeonCharacter opponent)
-	{
-		System.out.println(name + " swings a mighty sword at " +
-							opponent.getName() + ":");
-		super.attack(opponent);
-	}//end override of attack method
+    public void attack(DungeonCharacter opponent) {
+        System.out.println(name + " swings a mighty sword at " +
+                opponent.getName() + ":");
+        super.attack(opponent);
+    }
 
 
+    public void battleChoices(DungeonCharacter opponent) {
+        Scanner kb = new Scanner(System.in);
+        int choice;
 
+        super.battleChoices(opponent);
 
-    public void battleChoices(DungeonCharacter opponent)
-	{
-		Scanner kb = new Scanner(System.in);
-		int choice;
+        do {
+            System.out.println("1. Attack Opponent");
+            System.out.println("2. Crushing Blow on Opponent");
+            System.out.print("Choose an option: ");
+            choice = kb.nextInt();
 
-		super.battleChoices(opponent);
-
-		do
-		{
-		    System.out.println("1. Attack Opponent");
-		    System.out.println("2. Crushing Blow on Opponent");
-		    System.out.print("Choose an option: ");
-		    choice = kb.nextInt();
-
-		    switch (choice)
-		    {
-			    case 1: attack(opponent);
-			        break;
-			    case 2: crushingBlow(opponent);
-			        break;
-			    default:
-			        System.out.println("invalid choice!");
-		    }//end switch
-
-			numTurns--;
-			if (numTurns > 0)
-			    System.out.println("Number of turns remaining is: " + numTurns);
-
-		} while(numTurns > 0);
-
-    }//end battleChoices method
-
-}//end Hero class
+            switch (choice) {
+                case 1:
+                    attack(opponent);
+                    break;
+                case 2:
+                    crushingBlow(opponent);
+                    break;
+                default:
+                    System.out.println("invalid choice!");
+            }
+            numTurns--;
+            if (numTurns > 0)
+                System.out.println("Number of turns remaining is: " + numTurns);
+        } while (numTurns > 0);
+    }
+}
