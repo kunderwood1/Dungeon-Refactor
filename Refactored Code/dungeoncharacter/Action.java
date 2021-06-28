c6b5d933fe332d64b98275d1357ded54fdc63e70
Nursyahira Sabrina <nursyahiras12@gmail.com>
2021-06-28 23:21:28 +0800

    remove dungeon

--- Refactored Code/dungeoncharacter/Action.java
+++
@@ -1,8 +0,0 @@
-package dungeoncharacter;
-
-public interface Action {
-	public abstract int addHP(final int amountHealed);
-	public abstract int subHP(final int damageDealt);
-	public abstract void attack(final DungeonCharacter opponent);
-	public abstract void specialAttack(final DungeonCharacter oppontent);
-}
